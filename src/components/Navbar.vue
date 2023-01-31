8af97177ab5be102a7fd636b6cb8b3935d55a908
Your Name01~ <you@example.com>
2023-01-31 18:40:54 +0000

    fix: fix bug with fonts

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,13 @@
 <template >
   <nav class="nav">
     <article class="nav--logo">
-      <img src="/public/img/logo.svg" alt="logo">
+      <img src="/img/logo.svg" alt="logo">
       <span>vue weather</span>
     </article>
     <div class="nav--city">
-      <img src="/public/img/city.svg" alt="drop">
+      <img src="/img/city.svg" alt="drop">
       <select>
-        <option disabled>Выберите город</option>
+        <option disabled selected>Выберите город</option>
         <option value="Uzbekistan">Uzbekistan</option>
         <option value="UAE">UAE</option>
         <option value="Turky">Turky</option>
